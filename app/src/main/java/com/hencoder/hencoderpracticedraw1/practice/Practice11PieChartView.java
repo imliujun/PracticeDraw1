c3a64aba45ab9d559eb61eb465bd58b28cd99ad2
Liu Jun <mail@liujun.info>
2017-07-18 21:05:12 +0800

    完整饼图

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -36,13 +36,14 @@ public class Practice11PieChartView extends View {
             Color.LTGRAY };
 
     RectF mRectF;
-
+    RectF mMoveRectF;
     DisplayMetrics dm;
 
     int mDiameter;
 
     Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 
     {
@@ -53,6 +54,14 @@ public class Practice11PieChartView extends View {
         mDiameter = dm.widthPixels / 2;
         mRectF = new RectF(mDiameter / 2F, 50, mDiameter * 1.5F, mDiameter + 10);
 
+        mMoveRectF = new RectF(mRectF);
+        mMoveRectF.left -= 20;
+        mMoveRectF.right -= 20;
+        mMoveRectF.top -= 20;
+        mMoveRectF.bottom -= 20;
+
+        mLinePaint.setColor(Color.WHITE);
+        mLinePaint.setStrokeWidth(3);
         mTextPaint.setTextSize(24);
         mTextPaint.setColor(Color.WHITE);
     }
@@ -66,27 +75,49 @@ public class Practice11PieChartView extends View {
         //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
 
         float startAngle = 0;
+        int margin = 20;
+        float lineX;
+        float lineY;
+        float pointX;
+        float pointY;
+        float endLineX;
+        float textX;
+        RectF rectF;
         for (int i = 0; i < mNames.length; i++) {
             float sweepAngle = mHeight[i] * 360;
             mPaint.setColor(mColor[i]);
-            if (i == 5) {
-                RectF rectF = new RectF(mRectF);
-                rectF.left -= 20;
-                rectF.right -= 20;
-                rectF.top -= 20;
-                rectF.bottom -= 20;
-                canvas.drawArc(rectF, startAngle, sweepAngle, true, mPaint);
-            } else {
-                canvas.drawArc(mRectF, startAngle, sweepAngle, true, mPaint);
-            }
 
             float textAngle = startAngle + sweepAngle / 2;
 
-            float pointX = (float) (mRectF.centerX() + mRectF.width() / 2 *
+            if (i == 5) {
+                rectF = mMoveRectF;
+            } else {
+                rectF = mRectF;
+            }
+
+            canvas.drawArc(rectF, startAngle, sweepAngle, true, mPaint);
+            pointX = (float) (rectF.centerX() +
+                                      rectF.width() / 2 * Math.cos(textAngle * Math.PI / 180));
+            pointY = (float) (rectF.centerY() +
+                                      rectF.height() / 2 * Math.sin(textAngle * Math.PI / 180));
+            lineX = (float) (rectF.centerX() + (rectF.width() / 2 + margin) *
                     Math.cos(textAngle * Math.PI / 180));
-            float pointY = (float) (mRectF.centerY() + mRectF.height() / 2 *
+            lineY = (float) (rectF.centerY() + (rectF.height() / 2 + margin) *
                     Math.sin(textAngle * Math.PI / 180));
-            canvas.drawText(mNames[i], pointX, pointY, mTextPaint);
+
+            canvas.drawLine(pointX, pointY, lineX, lineY, mLinePaint);
+
+            if (textAngle >= 90 && textAngle <= 270) {
+                endLineX = lineX - 100;
+                textX = endLineX - 20;
+                mTextPaint.setTextAlign(Paint.Align.RIGHT);
+            } else {
+                endLineX = lineX + 100;
+                textX = endLineX + 20;
+                mTextPaint.setTextAlign(Paint.Align.LEFT);
+            }
+            canvas.drawLine(lineX, lineY, endLineX, lineY, mLinePaint);
+            canvas.drawText(mNames[i], textX, lineY + 3, mTextPaint);
 
             startAngle += sweepAngle + 2;
         }
